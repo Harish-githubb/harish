3c757be2ef8d70c15f7534043d1f89b9ee212444
Harish-githubb <boonlayharish@gmail.com>
2024-06-05 16:25:20 +0800

    server.py

--- server.py
+++ server.py
@@ -1,55 +1,28 @@
-from flask import *
+from flask import Flask, request, render_template, abort
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return render_template ("index.html")
-
+    # Serve the registration form
+    return render_template("index.html")
 
 @app.route('/Register', methods=["POST"])
-
 def Register():
-    data=request.form
-    Name=data["Name"]
-    Age=data["Age"]
-    phonenumber=data["Number"]
-    Email=data["Email"]
-    #Gender = "Male" if data.get("Male", 'off') == "on" else ("Female" if data.get("Female", 'off') == "on" else None)
-
-    infos= {
-       "Name":data.get("Name"),
-       "Age":data.get("Age"),
-       "Phonenumber":data.get("Number"),
-       "Email":data.get("Email"),
-       "Gender":"Male" if data.get("Male", 'off') == "on" else ("Female" if data.get("Female", 'off') == "on" else None)
-          }
-    # if data.get("Male","off")=="on":
-    #    Gender="Male"
-    # else:
-    #    Gender="Female"
-
-
-    # details = {
-    #         "Name": data.get("Name"),
-    #         "Age": data.get("Age"),
-    #         "PhoneNumber": data.get("Number"),
-    #         "Email": data.get("Email"),
-    #         "Gender": "Male" if data.get("Male", 'off') == "on" else ("Female" if data.get("Female", 'off') == "on" else None)
-    #     }
-    # details=[Name,Age,phonenumber,Email,Gender]
-    
-
-    return render_template("results.html", infos=infos)
-
-
-
-
-
+    if request.method == "POST":
+        data = request.form
+        infos = {
+            "Name": data.get("Name"),
+            "Age": data.get("Age"),
+            "Phonenumber": data.get("Number"),
+            "Email": data.get("Email"),
+            "Gender": "Male" if data.get("Male", 'off') == "on" else ("Female" if data.get("Female", 'off') == "on" else None)
+        }
+        # Render results page with collected information
+        return render_template("results.html", infos=infos)
+    else:
+        # If the wrong request method is used, return 405 Method Not Allowed
+        abort(405)
 
 if __name__ == "__main__":
-  app.run(host='127.0.0.1',port=5001,debug=True)
-
-
-
+    app.run(host='0.0.0.0', port=5678, debug=True)
